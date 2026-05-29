71db762a76c84a1d7b0dcbc64cc6fd545608b973
shuShanJing <3846414415@qq.com>
2026-05-29 13:58:02 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import time
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+# 注意： redis 服务器就是 docker-compose 里定义的服务名
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return f'Hello from Docker! 你是第 {count} 位访客。\n'
+
+if __name__ == "__main__":
+   app.run(host="0.0.0.0", port=5000) 
